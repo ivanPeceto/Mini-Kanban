42c8a349c41332a5e3caf0e27f132e9749d9a3e4
ivanleirbag <ivanpeceto20@gmail.com>
2025-09-05 23:01:48 -0300

    feat(front): Añade módulo create-task

--- frontend/src/app/create-task/create-task.ts
+++ frontend/src/app/create-task/create-task.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-task',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-task.html',
-  styleUrl: './create-task.css'
+  styleUrl: './create-task.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateTask {
+  create = output<{ title: string; description?: string }>();
+  cancel = output<void>();
 
+  private formbuilder = inject(FormBuilder);
+  taskForm = this.formbuilder.nonNullable.group({
+    title: ['', [Validators.required, Validators.maxLength(100)]],
+    description: ['', [Validators.maxLength(500)]]
+  });
+
+  onSubmit(): void {
+    if (this.taskForm.invalid) {
+      return;
+    }
+    this.create.emit(this.taskForm.getRawValue());
+  }
 }
