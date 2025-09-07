2a620376912aa042086028929e432d8e73ffea08
ivan <ivanpeceto20@gmail.com>
2025-09-07 17:00:08 -0300

    fix(front): Corrige bugs visuales del drag and drop

--- frontend/src/app/column/column.ts
+++ frontend/src/app/column/column.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject, input, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, output, signal } from '@angular/core';
 import { TaskCard } from '../task-card/task-card';
 import { Task } from '../shared/types';
 import { CreateTask } from '../create-task/create-task';
 import { TaskService } from '../task.service';
-import { DragDropModule } from "@angular/cdk/drag-drop";
+import { CdkDragDrop, DragDropModule } from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-column',
@@ -15,6 +15,11 @@ import { DragDropModule } from "@angular/cdk/drag-drop";
 export class Column {
   title = input.required<string>();
   tasks = input.required<Task[]>();
+  listId = input.required<string>();
+  listData = input.required<Task[]>();
+
+  dropped = output<CdkDragDrop<Task[]>>();
+
   isCreatingNewTask = signal(false);
 
   private taskService = inject(TaskService);
